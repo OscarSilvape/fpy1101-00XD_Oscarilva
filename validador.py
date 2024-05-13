7a5272b3d34f65e5fe25ca0bb526785dfd3fd654
OscarSilvape <168455268+OscarSilvape@users.noreply.github.com>
2024-05-12 22:38:48 -0400

    Add files via upload

---
+++ validador.py
@@ -0,0 +1,17 @@
+'Notas de los alumnos'
+
+dicc = {'Andrea': [5.3, 6.2, 6, 1.5],
+        'Benito': [7, 5.4, 4.3, 6, 7],
+        'Carlos': [4, 3.7, 7, 6, 5,5, 5, 4],
+        'Daniela': [4.8, 6.7, 3.9, 5.1, 6],
+        'Esteban': [5.6, 7.2, 6.1, 4.5, 6.9],
+        'Fabian': [5.5, 5.2, 5, 7, 4.9]}
+
+alumno = []
+promedio = []
+
+#GENERA AQUI EL PROCESO
+
+for ind in range(len(alumno)):
+    print(f'Alumno: {alumno[ind]} - Promedio: {promedio[ind]}')
+
